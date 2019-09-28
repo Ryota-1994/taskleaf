d691922f3c33dd9dee2df4be31b6272e3ff74ccc
Ryota-1994 <montanawao@gmail.com>
2019-09-28 19:54:05 +0900

    Add task registration function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,12 +2,24 @@ class TasksController < ApplicationController
   def index
   end
 
-  def show
+  def show 
   end
 
   def new
+    @task = Task.new
   end
 
   def edit
   end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:name, :description)
+    end
 end
