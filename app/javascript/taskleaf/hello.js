2a8a60a728838211d7babce4495784eee0134f38
Ryota-1994 <montanawao@gmail.com>
2019-10-14 19:02:03 +0900

    Finished Taskleaf

--- app/javascript/taskleaf/hello.js
+++ app/javascript/taskleaf/hello.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -7,3 +8,4 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.appendChild(document.createElement('div')),
   );
 });
+*/
